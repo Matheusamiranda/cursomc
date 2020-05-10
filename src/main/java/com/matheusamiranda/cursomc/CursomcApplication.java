adb7e7139be79ecccd8604120bf58a035260b6b0
Matheus Miranda <matheusamiranda@gmail.com>
2020-05-10 14:52:15 -0300

    configuracoes de lista de dados automatica

--- src/main/java/com/matheusamiranda/cursomc/CursomcApplication.java
+++ src/main/java/com/matheusamiranda/cursomc/CursomcApplication.java
@@ -1,13 +1,29 @@
 package com.matheusamiranda.cursomc;
 
+import com.matheusamiranda.cursomc.domain.Categoria;
+import com.matheusamiranda.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(1, "Informatica");
+        Categoria cat2 = new Categoria(2, "Escritorio");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
