5189d03979436369dd6e9a07614c10f15fc02518
Matheus Miranda <matheusamiranda@gmail.com>
2020-07-02 21:37:30 -0300

    Pedido e Pagamentos

---
+++ src/main/java/com/matheusamiranda/cursomc/domain/PagamentoComBoleto.java
@@ -0,0 +1,39 @@
+package com.matheusamiranda.cursomc.domain;
+
+import com.matheusamiranda.cursomc.domain.enums.EstadoPagamento;
+
+import javax.persistence.Entity;
+import java.util.Date;
+
+@Entity
+public class PagamentoComBoleto extends Pagamento{
+    private static final long serialVersionUID = 1L;
+
+    private Date dataVencimento;
+    private Date dataPagamento;
+
+    PagamentoComBoleto(){
+    }
+
+    public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataPagamento, Date dataVencimento) {
+        super(id, estado, pedido);
+        this.dataPagamento = dataPagamento;
+        this.dataVencimento = dataVencimento;
+    }
+
+    public Date getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(Date dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public Date getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(Date dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+}
---
+++ src/main/java/com/matheusamiranda/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,9 @@
+package com.matheusamiranda.cursomc.repositories;
+
+import com.matheusamiranda.cursomc.domain.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+}
