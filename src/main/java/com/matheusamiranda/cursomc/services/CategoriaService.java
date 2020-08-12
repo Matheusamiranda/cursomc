3b839d708c374be54eeba03946da717f97ae59e2
Matheus Miranda <matheusamiranda@gmail.com>
2020-08-11 22:10:54 -0300

    Metodos post, delete e put

--- src/main/java/com/matheusamiranda/cursomc/services/CategoriaService.java
+++ src/main/java/com/matheusamiranda/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.matheusamiranda.cursomc.services;
 
 import com.matheusamiranda.cursomc.domain.Categoria;
 import com.matheusamiranda.cursomc.repositories.CategoriaRepository;
+import com.matheusamiranda.cursomc.services.exceptions.DataIntegrityException;
 import com.matheusamiranda.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,4 +20,20 @@ public class CategoriaService {
         return obj.orElseThrow(() -> new ObjectNotFoundException( "Objeto não encontrado! Id: " + id + ", Tipo: "
                 + Categoria.class.getName()));
     }
-}
+    public Categoria insert(Categoria obj){
+        return repo.save(obj);
+    }
+
+    public Categoria update(Categoria obj){
+        find(obj.getId());
+        return repo.save(obj);
+    }
+
+    public void delete(Integer id){
+        find(id);
+        try{
+            repo.deleteById(id);
+        } catch (DataIntegrityException e){
+            throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+        }
+    }}
