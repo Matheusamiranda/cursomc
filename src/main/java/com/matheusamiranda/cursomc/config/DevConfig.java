2ec9bdb5a221a5856a60ae2b8c733a9f9c4bc9f1
Matheus Miranda <matheusamiranda@gmail.com>
2020-09-03 21:41:17 -0300

    configurações do application properties e envio de email

--- src/main/java/com/matheusamiranda/cursomc/config/DevConfig.java
+++ src/main/java/com/matheusamiranda/cursomc/config/DevConfig.java
@@ -1,6 +1,8 @@
 package com.matheusamiranda.cursomc.config;
 
 import com.matheusamiranda.cursomc.services.DBService;
+import com.matheusamiranda.cursomc.services.EmailService;
+import com.matheusamiranda.cursomc.services.SmtpEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -26,5 +28,10 @@ public class DevConfig {
         }
         dbService.instantiateTestDatabase();
         return true;
+    }
+
+    @Bean
+    public EmailService emailService(){
+        return new SmtpEmailService();
     }
 }
